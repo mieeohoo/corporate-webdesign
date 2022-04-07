035722f635cb6f7c673cfc77724f4e27cd2343f4
mieeohoo <87647608+mieeohoo@users.noreply.github.com>
2022-04-07 15:40:28 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+$(function(){
+
+  // ページトップボタン
+  var pagetop = $('#page-top');
+
+  pagetop.hide();
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 100){
+      pagetop.fadeIn();
+    } else {
+      pagetop.fadeOut();
+    }
+  });
+
+  pagetop.click(function(){
+    $('body, html').animate({scrollTop:0}, 300);
+    return false;
+  });
+});
